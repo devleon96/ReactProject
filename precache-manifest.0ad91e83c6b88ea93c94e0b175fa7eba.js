1feee55b193cd198e133d2808ed4fb09117770d0
Edwin Leon <eddy463@gmail.com>
2019-12-13 15:39:13 -0500

    Updates

---
+++ precache-manifest.0ad91e83c6b88ea93c94e0b175fa7eba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95ad77659cd7371ed847c274e878f40",
+    "url": "/ReactProject/index.html"
+  },
+  {
+    "revision": "23bcaee59d2f422092d7",
+    "url": "/ReactProject/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "2086644d971d4dff5f98",
+    "url": "/ReactProject/static/css/main.341493f1.chunk.css"
+  },
+  {
+    "revision": "23bcaee59d2f422092d7",
+    "url": "/ReactProject/static/js/2.d70b1753.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/ReactProject/static/js/2.d70b1753.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2086644d971d4dff5f98",
+    "url": "/ReactProject/static/js/main.cfe0c5b2.chunk.js"
+  },
+  {
+    "revision": "05bbacf69425f4ad64e2",
+    "url": "/ReactProject/static/js/runtime-main.156a2bcf.js"
+  }
+]);
